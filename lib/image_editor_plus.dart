67dd265e46e62fc13f504fffc5968c1d11a713af
Ananda S <4833102+asarangaram@users.noreply.github.com>
2024-04-06 15:17:57 +0530

    remove MultiImageEditor

--- lib/image_editor_plus.dart
+++ lib/image_editor_plus.dart
@@ -38,8 +38,8 @@ String i18n(String sourceString) =>
 
 /// Single endpoint for MultiImageEditor & SingleImageEditor
 class ImageEditor extends StatelessWidget {
-  final dynamic image;
-  final List? images;
+  final Uint8List image;
+
   final String? savePath;
   final int outputFormat;
 
@@ -55,8 +55,7 @@ class ImageEditor extends StatelessWidget {
 
   const ImageEditor({
     super.key,
-    this.image,
-    this.images,
+    required this.image,
     this.savePath,
     this.imagePickerOption = const o.ImagePickerOption(),
     this.outputFormat = o.OutputFormat.jpeg,
@@ -72,45 +71,20 @@ class ImageEditor extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (image == null &&
-        images == null &&
-        !imagePickerOption.captureFromCamera &&
-        !imagePickerOption.pickFromGallery) {
-      throw Exception(
-          'No image to work with, provide an image or allow the image picker.');
-    }
-
-    if (image != null) {
-      return SingleImageEditor(
-        image: image,
-        savePath: savePath,
-        imagePickerOption: imagePickerOption,
-        outputFormat: outputFormat,
-        cropOption: cropOption,
-        blurOption: blurOption,
-        brushOption: brushOption,
-        emojiOption: emojiOption,
-        filtersOption: filtersOption,
-        flipOption: flipOption,
-        rotateOption: rotateOption,
-        textOption: textOption,
-      );
-    } else {
-      return MultiImageEditor(
-        images: images ?? [],
-        savePath: savePath,
-        imagePickerOption: imagePickerOption,
-        outputFormat: outputFormat,
-        cropOption: cropOption,
-        blurOption: blurOption,
-        brushOption: brushOption,
-        emojiOption: emojiOption,
-        filtersOption: filtersOption,
-        flipOption: flipOption,
-        rotateOption: rotateOption,
-        textOption: textOption,
-      );
-    }
+    return SingleImageEditor(
+      image: image,
+      savePath: savePath,
+      imagePickerOption: imagePickerOption,
+      outputFormat: outputFormat,
+      cropOption: cropOption,
+      blurOption: blurOption,
+      brushOption: brushOption,
+      emojiOption: emojiOption,
+      filtersOption: filtersOption,
+      flipOption: flipOption,
+      rotateOption: rotateOption,
+      textOption: textOption,
+    );
   }
 
   static i18n(Map<String, String> translations) {
@@ -142,259 +116,6 @@ class ImageEditor extends StatelessWidget {
       bodyMedium: TextStyle(color: Colors.white),
     ),
   );
-}
-
-/// Show multiple image carousel to edit multple images at one and allow more images to be added
-class MultiImageEditor extends StatefulWidget {
-  final List images;
-  final String? savePath;
-  final int outputFormat;
-
-  final o.ImagePickerOption imagePickerOption;
-  final o.CropOption? cropOption;
-  final o.BlurOption? blurOption;
-  final o.BrushOption? brushOption;
-  final o.EmojiOption? emojiOption;
-  final o.FiltersOption? filtersOption;
-  final o.FlipOption? flipOption;
-  final o.RotateOption? rotateOption;
-  final o.TextOption? textOption;
-
-  const MultiImageEditor({
-    super.key,
-    this.images = const [],
-    this.savePath,
-    this.imagePickerOption = const o.ImagePickerOption(),
-    this.outputFormat = o.OutputFormat.jpeg,
-    this.cropOption = const o.CropOption(),
-    this.blurOption = const o.BlurOption(),
-    this.brushOption = const o.BrushOption(),
-    this.emojiOption = const o.EmojiOption(),
-    this.filtersOption = const o.FiltersOption(),
-    this.flipOption = const o.FlipOption(),
-    this.rotateOption = const o.RotateOption(),
-    this.textOption = const o.TextOption(),
-  });
-
-  @override
-  createState() => _MultiImageEditorState();
-}
-
-class _MultiImageEditorState extends State<MultiImageEditor> {
-  List<ImageItem> images = [];
-  PermissionStatus galleryPermission = PermissionStatus.permanentlyDenied,
-      cameraPermission = PermissionStatus.permanentlyDenied;
-
-  checkPermissions() async {
-    if (widget.imagePickerOption.pickFromGallery) {
-      galleryPermission = await Permission.photos.status;
-    }
-
-    if (widget.imagePickerOption.captureFromCamera) {
-      cameraPermission = await Permission.camera.status;
-    }
-
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    images = widget.images.map((e) => ImageItem(e)).toList();
-    checkPermissions();
-
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    viewportSize = MediaQuery.of(context).size;
-
-    return Theme(
-      data: ImageEditor.theme,
-      child: Scaffold(
-        key: scaffoldGlobalKey,
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          actions: [
-            const BackButton(),
-            const Spacer(),
-            if (images.length < widget.imagePickerOption.maxLength &&
-                widget.imagePickerOption.pickFromGallery)
-              Opacity(
-                opacity: galleryPermission.isPermanentlyDenied ? 0.5 : 1,
-                child: IconButton(
-                  padding: const EdgeInsets.symmetric(horizontal: 8),
-                  icon: const Icon(Icons.photo),
-                  onPressed: () async {
-                    if (await Permission.photos.isPermanentlyDenied) {
-                      openAppSettings();
-                    }
-
-                    var selected = await imagePicker.pickMultiImage(
-                      requestFullMetadata: false,
-                    );
-
-                    images.addAll(selected.map((e) => ImageItem(e)).toList());
-                    setState(() {});
-                  },
-                ),
-              ),
-            if (images.length < widget.imagePickerOption.maxLength &&
-                widget.imagePickerOption.captureFromCamera)
-              Opacity(
-                opacity: cameraPermission.isPermanentlyDenied ? 0.5 : 1,
-                child: IconButton(
-                  padding: const EdgeInsets.symmetric(horizontal: 8),
-                  icon: const Icon(Icons.camera_alt),
-                  onPressed: () async {
-                    if (await Permission.camera.isPermanentlyDenied) {
-                      openAppSettings();
-                    }
-
-                    var selected = await imagePicker.pickImage(
-                      source: ImageSource.camera,
-                    );
-
-                    if (selected == null) return;
-
-                    images.add(ImageItem(selected));
-                    setState(() {});
-                  },
-                ),
-              ),
-            IconButton(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              icon: const Icon(Icons.check),
-              onPressed: () async {
-                Navigator.pop(context, images);
-              },
-            ),
-          ],
-        ),
-        body: Column(
-          children: [
-            SizedBox(
-              height: 332,
-              width: double.infinity,
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    const SizedBox(width: 32),
-                    for (var image in images)
-                      Stack(children: [
-                        GestureDetector(
-                          onTap: () async {
-                            var img = await Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => SingleImageEditor(
-                                  image: image,
-                                  outputFormat: o.OutputFormat.jpeg,
-                                ),
-                              ),
-                            );
-
-                            // print(img);
-
-                            if (img != null) {
-                              image.load(img);
-                              setState(() {});
-                            }
-                          },
-                          child: Container(
-                            margin: const EdgeInsets.only(
-                                top: 32, right: 32, bottom: 32),
-                            width: 200,
-                            height: 300,
-                            decoration: BoxDecoration(
-                              color: Colors.transparent,
-                              border:
-                                  Border.all(color: Colors.white.withAlpha(80)),
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(4),
-                              child: Image.memory(
-                                image.bytes,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Positioned(
-                          top: 36,
-                          right: 36,
-                          child: Container(
-                            height: 32,
-                            width: 32,
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                              color: Colors.black.withAlpha(60),
-                              borderRadius: BorderRadius.circular(16),
-                            ),
-                            child: IconButton(
-                              iconSize: 20,
-                              padding: const EdgeInsets.all(0),
-                              onPressed: () {
-                                // print('removing');
-                                images.remove(image);
-                                setState(() {});
-                              },
-                              icon: const Icon(Icons.clear_outlined),
-                            ),
-                          ),
-                        ),
-                        if (widget.filtersOption != null)
-                          Positioned(
-                            bottom: 32,
-                            left: 0,
-                            child: Container(
-                              height: 38,
-                              width: 38,
-                              alignment: Alignment.center,
-                              decoration: BoxDecoration(
-                                color: Colors.black.withAlpha(100),
-                                borderRadius: const BorderRadius.only(
-                                  topRight: Radius.circular(19),
-                                ),
-                              ),
-                              child: IconButton(
-                                iconSize: 20,
-                                padding: const EdgeInsets.all(0),
-                                onPressed: () async {
-                                  Uint8List? editedImage = await Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => ImageFilters(
-                                        image: image.bytes,
-                                        options: widget.filtersOption,
-                                      ),
-                                    ),
-                                  );
-
-                                  if (editedImage != null) {
-                                    image.load(editedImage);
-                                  }
-
-                                  setState(() {});
-                                },
-                                icon: const Icon(Icons.photo_filter_sharp),
-                              ),
-                            ),
-                          ),
-                      ]),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  final imagePicker = ImagePicker();
 }
 
 /// Image editor with all option available
